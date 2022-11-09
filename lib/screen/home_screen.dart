13ed92a0aee24e0c4b2429c5b3ba895cd27cca26
olizyusuf <olizyusuf22@gmail.com>
2022-11-09 20:19:36 +0700

    add feature cant back

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,66 +20,104 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.only(right: 10, left: 10),
         child: Column(children: [
-          Container(
-            height: 190,
-            padding: const EdgeInsets.only(left: 5, right: 5),
-            child: Column(children: [
-              TextField(
-                controller: stockProvider.cCode,
-                decoration: const InputDecoration(
-                  labelText: 'Code',
-                  hintText: 'type code here or use scan cam',
-                ),
-                textInputAction: TextInputAction.next,
-                focusNode: stockProvider.myFocusNode,
-              ),
-              TextField(
-                controller: stockProvider.cQty,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Qty'),
-                textInputAction: TextInputAction.go,
-                onSubmitted: (value) {
-                  if (stockProvider.cCode.text.isNotEmpty &&
-                      stockProvider.cQty.text.isNotEmpty) {
-                    stockProvider.addData();
-                    stockProvider.clsText();
-                    stockProvider.myFocusNode.requestFocus();
-                  } else {
-                    sDialog(context);
-                  }
+          WillPopScope(
+            onWillPop: () async {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('Are you sure you want to quit!?'),
+                    content: const Text(
+                        'This proses will delete all data, you can export data first.'),
+                    actions: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context, true);
+                        },
+                        child: const Text('Export'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context, true);
+                        },
+                        child: const Text('Yes'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context, false);
+                        },
+                        child: const Text(
+                          'No',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      )
+                    ],
+                  );
                 },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        stockProvider.clsText();
-                      },
-                      child: const Text('Clear')),
-                  IconButton(
-                      onPressed: () {
-                        stockProvider.scanBarcodeCam();
-                      },
-                      icon: const Icon(Icons.qr_code_scanner)),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (stockProvider.cCode.text.isNotEmpty &&
-                            stockProvider.cQty.text.isNotEmpty) {
-                          stockProvider.addData();
+              ).then((value) => print(value));
+              return false;
+            },
+            child: Container(
+              height: 190,
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: Column(children: [
+                TextField(
+                  controller: stockProvider.cCode,
+                  decoration: const InputDecoration(
+                    labelText: 'Code',
+                    hintText: 'type code here or use scan cam',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  focusNode: stockProvider.myFocusNode,
+                ),
+                TextField(
+                  controller: stockProvider.cQty,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Qty'),
+                  textInputAction: TextInputAction.go,
+                  onSubmitted: (value) {
+                    if (stockProvider.cCode.text.isNotEmpty &&
+                        stockProvider.cQty.text.isNotEmpty) {
+                      stockProvider.addData();
+                      stockProvider.clsText();
+                      stockProvider.myFocusNode.requestFocus();
+                    } else {
+                      sDialog(context);
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
                           stockProvider.clsText();
-                          stockProvider.myFocusNode.requestFocus();
-                        } else {
-                          sDialog(context);
-                        }
-                      },
-                      child: const Text('Save Data')),
-                ],
-              ),
-            ]),
+                        },
+                        child: const Text('Clear')),
+                    IconButton(
+                        onPressed: () {
+                          stockProvider.scanBarcodeCam();
+                        },
+                        icon: const Icon(Icons.qr_code_scanner)),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (stockProvider.cCode.text.isNotEmpty &&
+                              stockProvider.cQty.text.isNotEmpty) {
+                            stockProvider.addData();
+                            stockProvider.clsText();
+                            stockProvider.myFocusNode.requestFocus();
+                          } else {
+                            sDialog(context);
+                          }
+                        },
+                        child: const Text('Save Data')),
+                  ],
+                ),
+              ]),
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
